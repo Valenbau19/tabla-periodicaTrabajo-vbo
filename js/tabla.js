6ceb86ad3eea006bb3e03f2bd9b27e8bce2e449d
Valen <valenbautistao38@gmail.com>
2024-07-08 07:14:06 -0500

    Version 1

---
+++ js/tabla.js
@@ -0,0 +1,15 @@
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.querySelectorAll('.element');
+
+    elements.forEach(element => {
+        element.addEventListener('click', () => {
+            const symbol = element.getAttribute('data-symbol');
+            const name = element.getAttribute('data-name');
+            const number = element.getAttribute('data-number');
+            const atomica= element.getAttribute('data-atomica');
+            alert(`Elemento: ${name}\nSímbolo: ${symbol}\nNúmero Atómico: ${number}\nmasa atomica: ${atomica}`);
+        });
+    });
+});
+*/
